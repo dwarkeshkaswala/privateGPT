d686629e1f8224180534ba3b59e8d25a592b4ff5
Dwarkesh Kaswala <45330333+dwarkeshkaswala@users.noreply.github.com>
2023-07-13 22:03:50 -0400

    Re created to talk with a specific file

--- ingest.py
+++ ingest.py
@@ -5,6 +5,7 @@ from typing import List
 from dotenv import load_dotenv
 from multiprocessing import Pool
 from tqdm import tqdm
+from chromadb.config import Settings
 
 from langchain.document_loaders import (
     CSVLoader,
@@ -24,14 +25,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
-from constants import CHROMA_SETTINGS
 
 
 load_dotenv()
 
 
 # Load environment variables
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
 source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
 chunk_size = 500
@@ -90,74 +89,59 @@ def load_single_document(file_path: str) -> List[Document]:
 
     raise ValueError(f"Unsupported file extension '{ext}'")
 
-def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
-    """
-    Loads all documents from the source documents directory, ignoring specified files
-    """
-    all_files = []
-    for ext in LOADER_MAPPING:
-        all_files.extend(
-            glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True)
-        )
-    filtered_files = [file_path for file_path in all_files if file_path not in ignored_files]
 
-    with Pool(processes=os.cpu_count()) as pool:
-        results = []
-        with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
-            for i, docs in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
-                results.extend(docs)
-                pbar.update()
 
-    return results
-
-def process_documents(ignored_files: List[str] = []) -> List[Document]:
+def process_documents(doc_path) -> List[Document]:
     """
     Load documents and split in chunks
     """
-    print(f"Loading documents from {source_directory}")
-    documents = load_documents(source_directory, ignored_files)
+    print(f"Loading document: {doc_path}")
+    documents = load_single_document(doc_path)
     if not documents:
         print("No new documents to load")
         exit(0)
-    print(f"Loaded {len(documents)} new documents from {source_directory}")
+    print(f"Loaded {len(documents)} new documents from {doc_path}")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     texts = text_splitter.split_documents(documents)
     print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
     return texts
 
-def does_vectorstore_exist(persist_directory: str) -> bool:
-    """
-    Checks if vectorstore exists
-    """
-    if os.path.exists(os.path.join(persist_directory, 'index')):
-        if os.path.exists(os.path.join(persist_directory, 'chroma-collections.parquet')) and os.path.exists(os.path.join(persist_directory, 'chroma-embeddings.parquet')):
-            list_index_files = glob.glob(os.path.join(persist_directory, 'index/*.bin'))
-            list_index_files += glob.glob(os.path.join(persist_directory, 'index/*.pkl'))
-            # At least 3 documents are needed in a working vectorstore
-            if len(list_index_files) > 3:
-                return True
-    return False
 
 def main():
     # Create embeddings
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    
 
-    if does_vectorstore_exist(persist_directory):
-        # Update and store locally vectorstore
-        print(f"Appending to existing vectorstore at {persist_directory}")
-        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-        collection = db.get()
-        texts = process_documents([metadata['source'] for metadata in collection['metadatas']])
-        print(f"Creating embeddings. May take some minutes...")
-        db.add_documents(texts)
+    files = os.listdir(source_directory)
+    if len(files) == 0:
+        print("No files found in source directory")
+        exit(0)
     else:
-        # Create and store locally vectorstore
-        print("Creating new vectorstore")
-        texts = process_documents()
-        print(f"Creating embeddings. May take some minutes...")
-        db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db = None
+        if not os.path.exists("db"):
+            os.makedirs("db")
+        for file in files:
+            folder = file.split(".")[0]
+            if not os.path.exists(f"db/{folder}"):
+                os.makedirs(f"db/{folder}")
+                print("Creating new vectorstore")
+                texts = process_documents(f"{source_directory}/{file}")
+                print(f"Creating embeddings. May take some minutes...")
+                CHROMA_SETTINGS = Settings(
+                    chroma_db_impl='duckdb+parquet',
+                    persist_directory = 'db/'+folder,
+                    anonymized_telemetry=False
+                )
+            
+                db = Chroma.from_documents(texts, embeddings, persist_directory="db/"+folder, client_settings=CHROMA_SETTINGS)
+            else:
+                if os.path.exists(os.path.join("db/"+folder, 'index')):
+                    if os.path.exists(os.path.join("db/"+folder, 'chroma-collections.parquet')) and os.path.exists(os.path.join("db/"+folder, 'chroma-embeddings.parquet')):
+                        list_index_files = glob.glob(os.path.join("db/"+folder, 'index/*.bin'))
+                        list_index_files += glob.glob(os.path.join("db/"+folder, 'index/*.pkl'))
+                        # At least 3 documents are needed in a working vectorstore
+                        if len(list_index_files) > 3:
+                            print("Vectorstore already exists. Skipping ingestion")
+
 
     print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
 
--- privateGPT.py
+++ privateGPT.py
@@ -8,6 +8,7 @@ from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
 import time
+from chromadb.config import Settings
 
 load_dotenv()
 
@@ -20,61 +21,76 @@ model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
-from constants import CHROMA_SETTINGS
+
 
 def main():
     # Parse the command line arguments
     args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case _default:
-            # raise exception if model_type is not supported
-            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
-        
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-        if query.strip() == "":
-            continue
+    
+    CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory = 'db/'+args.filename,
+        anonymized_telemetry=False
+    )
 
-        # Get the answer from the chain
-        start = time.time()
-        res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-        end = time.time()
+    if os.path.exists(CHROMA_SETTINGS.persist_directory):
+        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+        retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+        # activate/deactivate the streaming StdOut callback for LLMs
+        callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
+        # Prepare the LLM
+        match model_type:
+            case "LlamaCpp":
+                llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            case "GPT4All":
+                llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            case _default:
+                # raise exception if model_type is not supported
+                raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+            
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+        # Interactive questions and answers
+        while True:
+            query = input("\nEnter a query: ")
+            if query == "exit":
+                break
+            if query.strip() == "":
+                continue
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        print(answer)
+            # Get the answer from the chain
+            start = time.time()
+            res = qa(query)
+            answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+            end = time.time()
 
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+            # Print the result
+            print("\n\n> Question:")
+            print(query)
+            print(f"\n> Answer (took {round(end - start, 2)} s.):")
+            print(answer)
+
+            # Print the relevant sources used for the answer
+            for document in docs:
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+    else:
+        print("No database found. Please run the following command to create a database: \n python3 ingest.py")
+        exit(0)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
                                                  'using the power of LLMs.')
+    parser.add_argument("filename", help='Use this flag to specify filename (without extension) to talk with it.')
+
     parser.add_argument("--hide-source", "-S", action='store_true',
                         help='Use this flag to disable printing of source documents used for answers.')
 
     parser.add_argument("--mute-stream", "-M",
                         action='store_true',
                         help='Use this flag to disable the streaming StdOut callback for LLMs.')
+    
+    
 
     return parser.parse_args()
 
